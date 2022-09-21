1896b631319525e99095e8aa4db6a09ae6256fb4
Raghav Khullar <khullarraghav2001@gmail.com>
2022-09-22 01:05:15 +0530

    fix(UI): remove NITT logo from navbar

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,7 +1,13 @@
+import {
+	Box,
+	Image,
+} from "@chakra-ui/react";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { userContext } from "../../contexts/UserContext";
+import { BrowserView, MobileView } from "react-device-detect";
 import "./styles.css";
+import nittLogo from "../../../public/Assets/Images/NITT.png";
 const Landing = () => {
 	const navigate = useNavigate();
 	const { isLoggedIn } = useContext(userContext);
@@ -11,6 +17,16 @@ const Landing = () => {
 	return (
 		<>
 			<div className="title-box">
+				<MobileView>
+					<Box className="nittLogo">
+						<Image src={nittLogo} alt="NITT" />
+					</Box>
+				</MobileView>
+				<BrowserView>
+					<Box className="nittLogoBrowser">
+						<Image src={nittLogo} alt="NITT" />
+					</Box>
+				</BrowserView>
 				<div className="title-prefix">Nit Trichy&apos;s</div>
 				<div className="title">Festember</div>
 				<div className="title-suffix">The Sunken Paradise</div>
