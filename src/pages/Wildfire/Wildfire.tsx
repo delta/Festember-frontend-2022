2d889f047d250159534102c6a21550574fd92bae
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-09-25 00:07:19 +0530

    fix: outdated nav fixes (#41)

--- src/pages/Wildfire/Wildfire.tsx
+++ src/pages/Wildfire/Wildfire.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { config } from "../../../config";
 import { ListLayout } from "../../components";
 
 const issues = [
 	{
 		text: "Issue-1",
-		link: "/Assets/Documents/wildfire-issue-1.pdf",
+		link: config.basePath + "/Assets/Documents/wildfire-issue-1.pdf",
 	},
 	{
 		text: "Issue-2",
-		link: "/Assets/Documents/wildfire-issue-2.pdf",
+		link: config.basePath + "/Assets/Documents/wildfire-issue-2.pdf",
 	},
 	{
 		text: "Issue-3",
-		link: "/Assets/Documents/wildfire-issue-3.pdf",
+		link: config.basePath + "/Assets/Documents/wildfire-issue-3.pdf",
 	},
 ];
 
