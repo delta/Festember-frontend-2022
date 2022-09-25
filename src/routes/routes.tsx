8793162d0d1deac6f4a4fbee5520259c6c21754f
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-09-25 13:48:33 +0530

    fix: unprotected routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,9 +10,6 @@ import {
 	HowToReach,
 	FAQ,
 	Contact,
-	Events,
-	EventsPage,
-	Workshops,
 } from "../pages";
 import React from "react";
 
@@ -40,24 +37,6 @@ const routes: RouteType[] = [
 		path: "/sponsors",
 		description: "Sponsors of Festember 2022",
 		element: <Sponsors />,
-	},
-	{
-		title: "Events",
-		path: "/events",
-		description: "Event Clusters of Festember 2022",
-		element: <Events />,
-	},
-	{
-		title: "Events",
-		path: "/events/:cluster",
-		description: "Events Page of Festember 2022",
-		element: <EventsPage />,
-	},
-	{
-		title: "Workshops",
-		path: "/workshops",
-		description: "Workshops Page of Festember 2022",
-		element: <Workshops />,
 	},
 	{
 		title: "Wildfire",
