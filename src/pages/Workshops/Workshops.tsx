420e3e4e6c81d8938f015e305933562dedb16338
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-09-25 22:27:05 +0530

    fix: remove localstoraeg userid

--- src/pages/Workshops/Workshops.tsx
+++ src/pages/Workshops/Workshops.tsx
@@ -62,17 +62,13 @@ export default function Workshops() {
 		toast("Ticket Sales on September 26th and 27th", {
 			icon: "🎟️",
 			duration: 8000,
-			id: 'ticket-sales-toast'
+			id: "ticket-sales-toast",
 		});
 	}, [showTicketSaleToast]);
 
 	const handleWorkshopRegister = () => {
 		setIsRegisterLoading(true);
-		if (
-			!isLoggedIn ||
-			localStorage.getItem("userID") === null ||
-			localStorage.getItem("userID") === undefined
-		) {
+		if (!isLoggedIn) {
 			navigate("/login");
 			toast.error("Please login to register for a workshop");
 		} else if (choosenSlot === "") {
