fc13ee8d7dad1dd5aa84e58af87eb7ca7ba39d55
A Lokanush <78493749+A-Lokanush@users.noreply.github.com>
2022-09-29 19:23:34 +0530

    feat(profile): added user registered events (#71)

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -4,32 +4,39 @@ import { MobileView, BrowserView } from "react-device-detect";
 import { useState, useEffect, useContext, useCallback } from "react";
 import { Text } from '@chakra-ui/react';
 import { userContext } from "../../contexts/UserContext";
+import ReactFullpage from "@fullpage/react-fullpage";
+
 import { config } from "../../../config";
+
 const Profile = () => {
     const { isLoggedIn, loading } = useContext(userContext);
     const [qr, setQr] = useState<string>("");
-    const availableSizes = ["S", "M", "L", "XL", "XXL"];
-    const [size, setSize] = useState<string>("L");
-    const changeSize = (size: string) => {
-        setSize(size);
-    }
     const [userName, setUserName] = useState<string>("")
-    const [userID, setUserID] = useState<number>()
-    const getDetails = useCallback(() => {
-        fetch(`${config.backendOrigin}/user/details`, {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((res) => res.json()).
-            then((response) => setUserName(response.message.user_name))
-            .catch((e) => {
-                console.log(e);
+	const [eventDetails, setEventDetails] = useState([{}]);
+	const [workshopDetails, setWorkshopDetails] = useState([{}]);
+	const [informalDetails, setInformalDetails] = useState([{}]);
 
-            });
-    }, [userID]);
+    const getDetails = useCallback(() => {
+        fetch(`${config.backendOrigin}/user/registeredDetails`, {
+				method: "GET",
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json",
+				}
+			})
+				.then((res) => res.json())
+				.then((response) => {
+					if(response.status_code === 200){
+						setUserName(response.message.user_name);
+						setEventDetails(response.message.events);
+						setWorkshopDetails(response.message.workshops);
+						setInformalDetails(response.message.informals);
+					}
+				})
+				.catch((e) => {
+					console.log(e);
+				});
+    }, []);
 
     const getQR = () => {
 
@@ -57,84 +64,180 @@ const Profile = () => {
             getDetails();
         }
     }, [isLoggedIn, loading]);
+
     return (
-        <>
-            <MobileView>
-                <div className={styleMobile.display}>
-                    <div className={styleMobile.title}>Profile</div>
-                    <div className={styleMobile.profileDetails}>
+			<>
+				<MobileView>
+					<div className={styleMobile.display}>
+						<div className={styleMobile.title}>Profile</div>
+						<div className={styleMobile.profileDetails}>
+							<div className={styleMobile.name}>
+								{userName !== "" && <Text> {userName}</Text>}
+							</div>
 
-                        <div className={styleMobile.name}>{userName !== "" && (
-                            <Text> {userName}</Text>
-                        )}</div>
+							<div className={styleMobile.qr}>
+								{qr != "" && (
+									<img src={`data:image/png;base64,${qr}`} alt="QR" />
+								)}
+							</div>
 
-                        <div className={styleMobile.qr}>
-                            {qr != "" && (
-                                <img src={`data:image/png;base64,${qr}`} alt="QR" />
-                            )}
+							<div className={styleBrowser.event}>
+								<div className={styleMobile.subContainer}>
+									<div className={styleMobile.header}>
+										registered events
+									</div>
+									<div className={styleMobile.eventOptions}>
+										{eventDetails.map((i: any) => {
+											return (
+												<li className={styleMobile.available}>
+													{i.event_name}
+												</li>
+											);
+										})}
+									</div>
+								</div>
 
-                        </div>
+								<div className={styleMobile.subContainer}>
+									<div className={styleMobile.header}>
+										registered worshops
+									</div>
+									<div className={styleMobile.eventOptions}>
+										{workshopDetails.map((i: any) => {
+											return (
+												<li className={styleMobile.available}>
+													{i.workshop_name}
+												</li>
+											);
+										})}
+									</div>
+								</div>
 
-                        <div className={styleMobile.tshirt}>
-                            <div className={styleMobile.tshirtSize}>
-                                <div className={styleMobile.tshirtHeading}>T-Shirt Preference</div>
-                            </div>
-                            <div className={styleMobile.tshirtOptions}>
-                                <div className={styleMobile.currentSize}>{size}</div>
-                                <div className={styleMobile.allOptions}>
-                                    {availableSizes.map((i: string) => {
-                                        return (<div className={styleMobile.available} onClick={() => changeSize(i)}>{i}</div>);
-                                    })}
-                                </div>
+								<div className={styleMobile.subContainer} style={{marginBottom: "1em"}}>
+									<div className={styleMobile.header}>
+										registered informals
+									</div>
+									<div className={styleMobile.eventOptions}>
+										{informalDetails.map((i: any) => {
+											return (
+												<li className={styleMobile.available}>
+													{i.informals_name}
+												</li>
+											);
+										})}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</MobileView>
+				<BrowserView>
+					<ReactFullpage
+						scrollingSpeed={1000}
+						render={() => {
+							return (
+								<ReactFullpage.Wrapper>
+									<div className="section">
+										<div className={styleBrowser.display}>
+											<div className={styleBrowser.title}>
+												Profile
+											</div>
+											<div className={styleBrowser.profileDetails}>
+												<div className={styleBrowser.container}>
+													<div className={styleBrowser.name}>
+														{userName !== "" && (
+															<>
+																<Text> {userName}</Text>
+															</>
+														)}
+													</div>
 
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </MobileView>
-            <BrowserView>
-                <div className={styleBrowser.display}>
-                    <div className={styleBrowser.title}>Profile</div>
-                    <div className={styleBrowser.profileDetails}>
-                        <div className={styleBrowser.container}>
-                            <div className={styleBrowser.name}>
-                                {userName !== "" && (
-                                    <>
-                                    <Text> {userName}</Text>
-                                    </>)}
-                                
-                            </div>
-
-
-                            <div className={styleBrowser.qr}>
-                                {qr != "" && (
-                                    <img src={`data:image/png;base64,${qr}`} alt="QR" />
-                                )}
-
-                            </div>
-                        </div>
-                        <div className={styleBrowser.tshirt}>
-                            <div className={styleBrowser.dummyDiv}></div>
-                            <div className={styleBrowser.tshirtHeading}>T-Shirt Preference</div>
-
-                            <div className={styleBrowser.currentSize}><p>{size}</p></div>
-                        </div>
-                        <div className={styleBrowser.tshirtOptions}>
-
-                            {availableSizes.map((i: string) => {
-                                return (<div className={styleBrowser.available} onClick={() => changeSize(i)}>{i}</div>);
-                            })}
-
-
-
-                        </div>
-                    </div>
-                </div>
-            </BrowserView>
-
-        </>
-    )
+													<div className={styleBrowser.qr}>
+														{qr != "" && (
+															<img
+																src={`data:image/png;base64,${qr}`}
+																alt="QR"
+															/>
+														)}
+													</div>
+												</div>
+												<div className={styleBrowser.event}>
+													<div>
+														<div className={styleBrowser.header}>
+															registered events
+														</div>
+														<div
+															className={
+																styleBrowser.eventOptions
+															}
+														>
+															{eventDetails.map((event: any) => {
+																return (
+																	<div
+																		className={
+																			styleBrowser.available
+																		}
+																	>
+																		{event.event_name}
+																	</div>
+																);
+															})}
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div className="section">
+										<div
+											className={styleBrowser.title}
+											style={{ fontSize: "5em", marginTop: "-1em" }}
+										>
+											Registered Workshops and Informals
+										</div>
+										<div className={styleBrowser.other}>
+											<div className={styleBrowser.subContainer}>
+												<ul className={styleBrowser.list}>
+													{workshopDetails.map((field: any) => {
+														return (
+															<li>
+																<div
+																	className={
+																		styleBrowser.eventName
+																	}
+																>
+																	{field.workshop_name}
+																</div>
+															</li>
+														);
+													})}
+												</ul>
+											</div>
+											<div className={styleBrowser.subContainer}>
+												<ul className={styleBrowser.list}>
+													{informalDetails.map((field: any) => {
+														return (
+															<li>
+																<div
+																	className={
+																		styleBrowser.eventName
+																	}
+																>
+																	{field.informals_name}
+																</div>
+															</li>
+														);
+													})}
+												</ul>
+											</div>
+										</div>
+									</div>
+								</ReactFullpage.Wrapper>
+							);
+						}}
+					/>
+				</BrowserView>
+			</>
+		);
 }
 
 export default Profile;
