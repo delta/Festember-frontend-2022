2a77268b5a63b42f0dda29d7ead7647251fecf5a
Suhail Ahmed <suhailahmed2001sam@gmail.com>
2022-09-29 08:06:33 +0530

    fix: sponsors images basepath

--- src/pages/Sponsors/assets/content.ts
+++ src/pages/Sponsors/assets/content.ts
@@ -1,95 +1,97 @@
+import { config } from "../../../../config";
+
 export const contentUp: any = [
-    {
-        companyName:"Chennaiyil.com",
-        src:"/Assets/Images/SponsorImages/ChennaiNews.png",
-        portfolio:"Chennai News Partner"
-    },
-    {
-        companyName:"AGS Cinemas",
-        src:"/Assets/Images/SponsorImages/CinePartner.png",
-        portfolio:"Cine Partner"
-    },
-    {
-        companyName:"KalviMalar",
-        src:"/Assets/Images/SponsorImages/DailyEducationalPartner.png",
-        portfolio:"Daily Educational News Partner"
-    },
-    {
-        companyName:"Shortfundly",
-        src:"/Assets/Images/SponsorImages/DigitalStreamingPartner.png",
-        portfolio:"Digital Streaming Partner"
-    },
-    {
-        companyName:"Mirror Now",
-        src:"/Assets/Images/SponsorImages/MediaPartner.png",
-        portfolio:"Media Partner"
-    },
-    {
-        companyName:"Business Trichy",
-        src:"/Assets/Images/SponsorImages/NammaMagazinePartner.png",
-        portfolio:"Namma Magazine Partner"
-    },
-    {
-        companyName:"Woman's Era",
-        src:"/Assets/Images/SponsorImages/OfficialLifestylePartner.png",
-        portfolio:"Official Lifestyle Partner"
-    },
-    {
-        companyName:"Movies Now",
-        src:"/Assets/Images/SponsorImages/PresentingPartner.png",
-        portfolio:"Presenting Partner"
-    },
-    {
-        companyName:"Puthiya Thalamurai",
-        src:"/Assets/Images/SponsorImages/PressPartnerPuthiyaThalamurai.png",
-        portfolio:"Press Partner"
-    }
+	{
+		companyName: "Chennaiyil.com",
+		src: "/Assets/Images/SponsorImages/ChennaiNews.png",
+		portfolio: "Chennai News Partner",
+	},
+	{
+		companyName: "AGS Cinemas",
+		src: "/Assets/Images/SponsorImages/CinePartner.png",
+		portfolio: "Cine Partner",
+	},
+	{
+		companyName: "KalviMalar",
+		src: "/Assets/Images/SponsorImages/DailyEducationalPartner.png",
+		portfolio: "Daily Educational News Partner",
+	},
+	{
+		companyName: "Shortfundly",
+		src: "/Assets/Images/SponsorImages/DigitalStreamingPartner.png",
+		portfolio: "Digital Streaming Partner",
+	},
+	{
+		companyName: "Mirror Now",
+		src: "/Assets/Images/SponsorImages/MediaPartner.png",
+		portfolio: "Media Partner",
+	},
+	{
+		companyName: "Business Trichy",
+		src: "/Assets/Images/SponsorImages/NammaMagazinePartner.png",
+		portfolio: "Namma Magazine Partner",
+	},
+	{
+		companyName: "Woman's Era",
+		src: "/Assets/Images/SponsorImages/OfficialLifestylePartner.png",
+		portfolio: "Official Lifestyle Partner",
+	},
+	{
+		companyName: "Movies Now",
+		src: "/Assets/Images/SponsorImages/PresentingPartner.png",
+		portfolio: "Presenting Partner",
+	},
+	{
+		companyName: "Puthiya Thalamurai",
+		src: "/Assets/Images/SponsorImages/PressPartnerPuthiyaThalamurai.png",
+		portfolio: "Press Partner",
+	},
 ];
 
-export const contentDown:any = [
-    {
-        companyName:"Makkal TV",
-        src:"/Assets/Images/SponsorImages/PressPartnerMakkalTV.png",
-        portfolio:"Press Partner"
-    },
-    {
-        companyName:"HaramiPUN",
-        src:"/Assets/Images/SponsorImages/PunPartner.png",
-        portfolio:"PUN Partner"
-    },
-    {
-        companyName:"Suryan FM",
-        src:"/Assets/Images/SponsorImages/RadioPartner.png",
-        portfolio:"Radio Partner"
-    },
-    {
-        companyName:"The Logical Indian",
-        src:"/Assets/Images/SponsorImages/StoryPartner.png",
-        portfolio:"Story Partner"
-    },
-    {
-        companyName:"The Week",
-        src:"/Assets/Images/SponsorImages/WeeklyNewsPartner.png",
-        portfolio:"Weekly News Partner"
-    },
-    {
-        companyName:"Youth Incorporated",
-        src:"/Assets/Images/SponsorImages/YouthMediaPartner.png",
-        portfolio:"Youth Media Partner"
-    },
-    {
-        companyName:"Fever FM",
-        src:"/Assets/Images/SponsorImages/EnjoymentPartner.jpeg",
-        portfolio:"Enjoyment Partner"
-    },
-    {
-        companyName:"Dinamalar",
-        src:"/Assets/Images/SponsorImages/DailyNewsPartner.png",
-        portfolio:"Daily News Partner"
-    },
-    {
-        companyName:"Awesong",
-        src:"/Assets/Images/SponsorImages/MusicStreamingPartner.png",
-        portfolio:"Music Streaming Partner"
-    }
+export const contentDown: any = [
+	{
+		companyName: "Makkal TV",
+		src: "/Assets/Images/SponsorImages/PressPartnerMakkalTV.png",
+		portfolio: "Press Partner",
+	},
+	{
+		companyName: "HaramiPUN",
+		src: "/Assets/Images/SponsorImages/PunPartner.png",
+		portfolio: "PUN Partner",
+	},
+	{
+		companyName: "Suryan FM",
+		src: "/Assets/Images/SponsorImages/RadioPartner.png",
+		portfolio: "Radio Partner",
+	},
+	{
+		companyName: "The Logical Indian",
+		src: "/Assets/Images/SponsorImages/StoryPartner.png",
+		portfolio: "Story Partner",
+	},
+	{
+		companyName: "The Week",
+		src: "/Assets/Images/SponsorImages/WeeklyNewsPartner.png",
+		portfolio: "Weekly News Partner",
+	},
+	{
+		companyName: "Youth Incorporated",
+		src: "/Assets/Images/SponsorImages/YouthMediaPartner.png",
+		portfolio: "Youth Media Partner",
+	},
+	{
+		companyName: "Fever FM",
+		src: "/Assets/Images/SponsorImages/EnjoymentPartner.jpeg",
+		portfolio: "Enjoyment Partner",
+	},
+	{
+		companyName: "Dinamalar",
+		src: "/Assets/Images/SponsorImages/DailyNewsPartner.png",
+		portfolio: "Daily News Partner",
+	},
+	{
+		companyName: "Awesong",
+		src: "/Assets/Images/SponsorImages/MusicStreamingPartner.png",
+		portfolio: "Music Streaming Partner",
+	},
 ];
