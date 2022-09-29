003a6af87f47bbf39a29872e182523589654ace1
Suhail Ahmed <suhailahmed2001sam@gmail.com>
2022-09-30 00:38:15 +0530

    Fix: Guest Lectures

--- src/pages/GuestLectures/assets/content.ts
+++ src/pages/GuestLectures/assets/content.ts
@@ -1,30 +1,32 @@
-export const content = [
+export const content: GuestLecturesDetailsType[] = [
 	{
-		guestLectures_id: 1,
 		guestLectures_name: "Adah Sharma",
 		guestLectures_desc:
 			"An actress who has delivered innumerable hits like 1920, Commando 2, and S/O Sathyamoorthy, Adah Sharma has chiselled herself in the industry with her charisma. Don't miss her in Festember'22 at 9.30 AM on September 29 at Barn Hall as a part of the Carpe Diem series.",
-		guestLectures_image_name: "www.a.com",
+		guestLectures_image_name: "adah-sharma.jpg",
 	},
 	{
-		guestLectures_id: 2,
 		guestLectures_name: "Vishal Chandrasekhar",
 		guestLectures_desc:
 			"From starting to play the keyboard at the young age of 6 to giving solo shows by the time he was 10, Vishal Chandrasekhar's pathway to success is a remarkable and inspiring journey. He is known for drawing the attention of music lovers using his creative side in numerous movies, including the sensational “Sita Ramam,” “Oh Manapenne,” “Padi Padi Leche Manasu,” and other countless hits. Catch him in Festember’22 at 6:00 pm on the 29th of September as a part of the Carpe Diem Series in A13 Hall.",
-		guestLectures_image_name: "www.a.com",
+		guestLectures_image_name: "vishal-chandrasekhar.jpg",
 	},
 	{
-		guestLectures_id: 3,
 		guestLectures_name: "Ashwin Saravanan",
 		guestLectures_desc:
 			"Ashwin Saravanan, a promising director, screenplay, and story writer in Kollywood, has delivered hits like Maya and Game Over. Next on his line is Connect, with Lady Superstar Nayanthara in the lead. His works have set a benchmark in the industry and have never failed to impress the audience. Don't miss him at Festember '22  9.30 AM on 30th September, A13 Hall. Let's brew the best ideas out of him!",
-		guestLectures_image_name: "www.a.com",
+		guestLectures_image_name: "ashwin-saravanan.jpg",
 	},
 	{
-		guestLectures_id: 4,
 		guestLectures_name: "Dhruv Vikram",
 		guestLectures_desc:
 			"From giving a captivating debut in Adithya Varma to delivering a fiery performance alongside his father in Mahaan, Dhruv Vikram is spellbinding his audience with stellar performances as an actor, singer and a lyricist. He has also enthralled us as a director for the short film 'Good Night Charlie' on child abuse and his new music video 'Manase'. Don't miss him at Festember '22 on 2nd October, 5 pm at Barn Hall to witness him speaking on his works!",
-		guestLectures_image_name: "www.a.com",
+		guestLectures_image_name: "dhruv-vikram.png",
+	},
+	{
+		guestLectures_name: "Meena Kandasamy",
+		guestLectures_desc:
+			"Ilavenil Meena Kandasamy (born 1984) is an Indian poet, fiction writer, translator and activist from Chennai, Tamil Nadu, India. Meena published two collections of poetry, Touch (2006) and Ms. Militancy (2010). From 2001-2002, she edited The Dalit, a bi-monthly alternative English magazine of the Dalit Media Network. She represented India at the University of Iowa's International Writing Program and was a Charles Wallace India Trust Fellow at the University of Kent, Canterbury, United Kingdom. She writes columns for platforms like Outlook India and The Hindu.",
+		guestLectures_image_name: "meena-kandasamy.jpg",
 	},
 ];
--- src/pages/GuestLectures/types.d.ts
+++ src/pages/GuestLectures/types.d.ts
@@ -1,21 +1,5 @@
 interface GuestLecturesDetailsType {
-	guestLectures_id: number;
 	guestLectures_name: string;
 	guestLectures_desc: string;
 	guestLectures_image_name: string;
-}
-
-interface SlotDetailsType {
-	slot_id: number;
-	workshop_id: number;
-	workshop_date: string;
-	slot_start_time: string;
-	slot_end_time: string;
-}
-
-interface InformalsDetailsType {
-	informals_id: number;
-	informals_name: string;
-	informals_desc: string;
-	informals_image_name: string;
 }
