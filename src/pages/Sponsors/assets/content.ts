6e8f1eb087a4de5dec121748cee5281d64d26f31
Raghav Khullar <khullarraghav2001@gmail.com>
2022-10-20 04:38:40 +0530

    feat: add sponsors content

--- src/pages/Sponsors/assets/content.ts
+++ src/pages/Sponsors/assets/content.ts
@@ -1,117 +1,459 @@
 import { config } from "../../../../config";
 
-export const contentUp: any = [
-	{
-		companyName: "AGS Cinemas",
-		src: config.basePath + "/Assets/Images/SponsorImages/CinePartner.png",
-		portfolio: "Cine Partner",
-	},
-	{
-		companyName: "Suryan FM",
-		src: config.basePath + "/Assets/Images/SponsorImages/RadioPartner.png",
-		portfolio: "Radio Partner",
-	},
-	{
-		companyName: "Movies Now",
-		src:
-			config.basePath + "/Assets/Images/SponsorImages/PresentingPartner.png",
-		portfolio: "Presenting Partner",
-	},
-	{
-		companyName: "Mirror Now",
-		src: config.basePath + "/Assets/Images/SponsorImages/MediaPartner.png",
-		portfolio: "Media Partner",
-	},
-	{
-		companyName: "The Week",
-		src:
-			config.basePath + "/Assets/Images/SponsorImages/WeeklyNewsPartner.png",
-		portfolio: "Weekly News Partner",
-	},
-	{
-		companyName: "Puthiya Thalamurai",
-		src:
-			config.basePath +
-			"/Assets/Images/SponsorImages/PressPartnerPuthiyaThalamurai.png",
-		portfolio: "Press Partner",
-	},
-	{
-		companyName: "Makkal TV",
-		src:
-			config.basePath +
-			"/Assets/Images/SponsorImages/PressPartnerMakkalTV.png",
-		portfolio: "Press Partner",
-	},
-	{
-		companyName: "Shortfundly",
-		src:
-			config.basePath +
-			"/Assets/Images/SponsorImages/DigitalStreamingPartner.png",
-		portfolio: "Digital Streaming Partner",
-	},
-	{
-		companyName: "Fever FM",
-		src:
-			config.basePath + "/Assets/Images/SponsorImages/EnjoymentPartner.jpeg",
-		portfolio: "Enjoyment Partner",
-	},
+const sponsors: any = [
+	[
+		{
+			name: "RECAL",
+			portfolio: "Nostalgia Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+	],
+	[
+		{
+			name: "Ubon Headphones",
+			portfolio: "Title Sponsor",
+			src: config.basePath + "/Assets/Images/SponsorImages/UBON.png",
+		},
+	],
+	[
+		{
+			name: "Royal Enfield",
+			portfolio: "Major Sponsor",
+			src: config.basePath + "/Assets/Images/SponsorImages/RoyalEnfield.png",
+		},
+		{
+			name: "BharatX",
+			portfolio: "Co-sponsored by",
+			src: config.basePath + "/Assets/Images/SponsorImages/BharatX.png",
+		},
+		{
+			name: "Hirect",
+			portfolio: "Powered by",
+			src: config.basePath + "/Assets/Images/SponsorImages/Hirect.png",
+		},
+	],
+];
 
+const proshowSponsors: any = [
+	[
+		{
+			name: "RealMe",
+			portfolio: "Popular Nite Title Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+		{
+			name: "KTM",
+			portfolio: "Adventure Bikes Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+		{
+			name: "Britannia Industries",
+			portfolio: "Comedy Nite Title Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/BritanniaIndustries.png",
+		},
+	],
+	[
+		{
+			name: "Cothas Coffee",
+			portfolio: "Coffee Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/CothasCoffee.jpg",
+		},
+		{
+			name: "City Union Bank",
+			portfolio: "Social Responsibility Partner",
+			src:
+				config.basePath + "/Assets/Images/SponsorImages/CityUnionBank.png",
+		},
+		{
+			name: "Coca Cola",
+			portfolio: "Beverage Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/CocaCola.png",
+		},
+	],
+];
 
-]
-export const contentDown: any = [
+const bankingSponsors: any = [
+	[
+		{
+			name: "Canara Bank",
+			portfolio: "Student Loans Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/CanaraBank.jpeg",
+		},
+		{
+			name: "State Bank of India",
+			portfolio: "YONO Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+		{
+			name: "Union Bank of India",
+			portfolio: "Finance Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/UnionBankofIndia.jpeg",
+		},
+	],
+	[
+		{
+			name: "Bank of Baroda",
+			portfolio: "Financial Services Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/BankOfBaroda.png",
+		},
+		{
+			name: "ICICI Bank",
+			portfolio: "Banking Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+	],
+];
+
+const fashionSponsors: any = [
+	[
+		{
+			name: "Emirates Modelling Company",
+			portfolio: "Modelling Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Emirates.jpg",
+		},
+		{
+			name: "Gobuyy",
+			portfolio: "Fashion Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Gobuyy.jpg",
+		},
+		{
+			name: "ParX",
+			portfolio: "Casual Clothing Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/ParX.jpg",
+		},
+	],
+	[
+		{
+			name: "Beardo",
+			portfolio: "Mens Grooming Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Beardo.png",
+		},
+		{
+			name: "Gatsby",
+			portfolio: "Styling Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Gatsby.jpg",
+		},
+		{
+			name: "Derby",
+			portfolio: "Clothing partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Derby.jpg",
+		},
+	],
+	[
+		{
+			name: "Van Heusen",
+			portfolio: "Apparels Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/VanHeusen.jpeg",
+		},
+		{
+			name: "Twin Birds",
+			portfolio: "Apparel Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/TwinBirds.jpeg",
+		},
+		{
+			name: "ayesha",
+			portfolio: "Accessories Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Ayesha.jpg",
+		},
+	],
+	[
+		{
+			name: "xctasy",
+			portfolio: "Jewellery Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/xctasy.jpeg",
+		},
+		{
+			name: "Reliance Trends",
+			portfolio: "Trends Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Trends.jpg",
+		},
+		{
+			name: "Wristbandwala",
+			portfolio: "Wearables Partner",
+			src:
+				config.basePath + "/Assets/Images/SponsorImages/Wristbandwala.png",
+		},
+	],
+];
+
+const educationSponsor: any = [
+	[
+		{
+			name: "Ace Engineering Academy",
+			portfolio: "GATE Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/AceEngineeringAcademy.png",
+		},
+		{
+			name: "TIME",
+			portfolio: "Crack CAT Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+		{
+			name: "Byju's Careerlabs",
+			portfolio: "Higher Education Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/Byju'sCareerlabs.png",
+		},
+	],
+	[
+		{
+			name: "SafeXpress",
+			portfolio: "Logistics Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/SafeXpress.png",
+		},
+		{
+			name: "Scholastic Publications",
+			portfolio: "English Lits Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Scholastic.jpg",
+		},
+		{
+			name: "Indian Oil Corporation Limited",
+			portfolio: "Industrial Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+	],
+	[
+		{
+			name: "Shankar IAS Academy",
+			portfolio: "Administrative Education Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/ShankarIASAcademy.jpg",
+		},
+		{
+			name: "Qshala",
+			portfolio: "Curiosity Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Qshala.png",
+		},
+		{
+			name: "Elearnmarkets",
+			portfolio: "Financial Education Partner",
+			src:
+				config.basePath + "/Assets/Images/SponsorImages/Elearnmarkets.jpg",
+		},
+	],
+];
+
+const additonalSponsor: any = [
+	[
+		{
+			name: "YiPPee!",
+			portfolio: "Food Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Yipeee.png",
+		},
+		{
+			name: "Nabati",
+			portfolio: "Wafer Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Nabati.png",
+		},
+		{
+			name: "StuCred",
+			portfolio: "Student Credit Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/StuCred.png",
+		},
+	],
+	[
+		{
+			name: "Gone Mad",
+			portfolio: "Choco Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/GoneMad.jpg",
+		},
+		{
+			name: "Cavins",
+			portfolio: "Refreshments Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Cavins.jpeg",
+		},
+		{
+			name: "Barbeque Nation",
+			portfolio: "Food Partner",
+			src:
+				config.basePath + "/Assets/Images/SponsorImages/BarbequeNation.png",
+		},
+	],
+	[
+		{
+			name: "Cornados",
+			portfolio: "Snacks Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Cornados.jpeg",
+		},
+		{
+			name: "Firstcry",
+			portfolio: "Toddler Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+		{
+			name: "Bharath Snacks",
+			portfolio: "Chikki Partner",
+			src:
+				config.basePath + "/Assets/Images/SponsorImages/BharathSnacks.jpg",
+		},
+	],
+];
+
+const photographySponsors: any = [
+	[
+		{
+			name: "Sony",
+			portfolio: "Camera Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/SONY.png",
+		},
+		{
+			name: "KAPA",
+			portfolio: "Capture Partner ",
+			src: config.basePath + "/Assets/Images/SponsorImages/KAPA.png",
+		},
+		{
+			name: "Fujifilm",
+			portfolio: "Photography Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/FujiFilm.png",
+		},
+		{
+			name: "Indian Institute of Photography",
+			portfolio: "Photography Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages",
+		},
+	],
+	[
+		{
+			name: "Nestaway",
+			portfolio: "Stay Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Nestaway.png",
+		},
+		{
+			name: "Havells",
+			portfolio: "Appliance Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Havells.png",
+		},
+		{
+			name: "Terence Lewis Dance Academy",
+			portfolio: "Dance Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/TerenceLewisDanceAcademy.jpeg",
+		},
+		{
+			name: "Hyperstack",
+			portfolio: "Certificate Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Hyperstack.png",
+		},
+	],
+];
+
+const gamingPartners: any = [
+	[
+		{
+			name: "ASUS",
+			portfolio: "Gaming Cluster Title Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/ASUS.jpg",
+		},
+		{
+			name: "BalleBaazi",
+			portfolio: "Cricket Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/BalleBaazi.jpeg",
+		},
+	],
+];
+
+const outreachSponsor: any = [
+	[
+		{
+			name: "Louis Music Point",
+			portfolio: "Musical Instruments Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/LouisMusicPoint.png",
+		},
+		{
+			name: "Unwind Centre",
+			portfolio: "Official GigaHertz Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Unwind.png",
+		},
+		{
+			name: "Diorama International Film Festival & market",
+			portfolio: "Film partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Diorama.jpg",
+		},
+		{
+			name: "Swarnabhoomi Academy of Music",
+			portfolio: "Melody Partner",
+			src:
+				config.basePath +
+				"/Assets/Images/SponsorImages/SwarnabhoomiAcademy.png",
+		},
+	],
+];
+
+const socialMediaSponsor: any = [
+	[
+		{
+			name: "Cherry store",
+			portfolio: "Unisex Jewellery partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/CherryStore.png",
+		},
+		{
+			name: "Paperish",
+			portfolio: "Gift Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Paperish.jpg",
+		},
+		{
+			name: "Craftscastle",
+			portfolio: "Crafts Castle",
+			src: config.basePath + "/Assets/Images/SponsorImages/CraftsCastle.jpg",
+		},
+		{
+			name: "Handlagetta",
+			portfolio: "Handmade Gifts Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/Handlagetta.png",
+		},
+	],
+];
+
+export const list: any = [
 	{
-		companyName: "Woman's Era",
-		src:
-			config.basePath +
-			"/Assets/Images/SponsorImages/OfficialLifestylePartner.png",
-		portfolio: "Official Lifestyle Partner",
+		title: "Sponsors",
+		data: sponsors,
 	},
 	{
-		companyName: "Dinamalar",
-		src:
-			config.basePath + "/Assets/Images/SponsorImages/DailyNewsPartner.png",
-		portfolio: "Daily News Partner",
+		title: "Proshow Sponsors",
+		data: proshowSponsors,
 	},
 	{
-		companyName: "KalviMalar",
-		src:
-			config.basePath +
-			"/Assets/Images/SponsorImages/DailyEducationalPartner.png",
-		portfolio: "Daily Educational News Partner",
+		title: "Banking Sponsors",
+		data: bankingSponsors,
 	},
 	{
-		companyName: "The Logical Indian",
-		src: config.basePath + "/Assets/Images/SponsorImages/StoryPartner.png",
-		portfolio: "Story Partner",
+		title: "Fashion Sponsors",
+		data: fashionSponsors,
 	},
 	{
-		companyName: "Business Trichy",
-		src:
-			config.basePath +
-			"/Assets/Images/SponsorImages/NammaMagazinePartner.png",
-		portfolio: "Namma Magazine Partner",
+		title: "Education Sponsors",
+		data: educationSponsor,
 	},
 	{
-		companyName: "HaramiPUN",
-		src: config.basePath + "/Assets/Images/SponsorImages/PunPartner.png",
-		portfolio: "PUN Partner",
+		title: "Additional Sponsors",
+		data: additonalSponsor,
 	},
 	{
-		companyName: "Youth Incorporated",
-		src:
-			config.basePath + "/Assets/Images/SponsorImages/YouthMediaPartner.png",
-		portfolio: "Youth Media Partner",
+		title: "Photography Sponsors",
+		data: photographySponsors,
 	},
 	{
-		companyName: "Awesong",
-		src:
-			config.basePath +
-			"/Assets/Images/SponsorImages/MusicStreamingPartner.png",
-		portfolio: "Music Streaming Partner",
+		title: "Gaming Partners",
+		data: gamingPartners,
 	},
 	{
-		companyName: "Chennaiyil.com",
-		src: config.basePath + "/Assets/Images/SponsorImages/ChennaiNews.png",
-		portfolio: "Chennai News Partner",
+		title: "Outreach Sponsors",
+		data: outreachSponsor,
+	},
+	{
+		title: "Social Media Sponsors",
+		data: socialMediaSponsor,
 	},
 ];
