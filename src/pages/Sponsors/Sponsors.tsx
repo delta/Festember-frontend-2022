871a2130c5c1ee9f8c01252f1c19288d6f0f4e8d
Raghav Khullar <khullarraghav2001@gmail.com>
2022-11-14 04:31:54 +0530

    feat(UI): change mobile UI of sponsors page, seed missing data

--- src/pages/Sponsors/Sponsors.tsx
+++ src/pages/Sponsors/Sponsors.tsx
@@ -1,13 +1,16 @@
 import { Center, Flex, Text } from "@chakra-ui/react";
 import { BrowserView, MobileView } from "react-device-detect";
 import styles from "./styles.module.css";
+import mobileStyles from "./mobileStyles.module.css";
 import { list } from "./assets/content";
 import SponsorCarousel from "../../components/SponsorCarousel/SponsorCarousel";
 
 const Sponsors = () => {
 	return (
 		<>
+		
 			<MobileView>
+			<h2 className={mobileStyles.heading}>SPONSORS</h2>
 				{/* <div className={styles.mobileContainer}>
 					<Text className={styles.name}>SPONSORS</Text>
 					<Flex p={3}>
@@ -17,8 +20,59 @@ const Sponsors = () => {
 						/>
 					</Flex>
 				</div> */}
+				<div className={mobileStyles.container}>
+					{list.map((item: any) => {
+						return (
+							<>
+								<div className={mobileStyles.title}>{item.title}</div>
+
+								{item.data.map((data: any) => {
+									return (
+										<>
+											{data.map((i: any) => {
+												return (
+													<>
+														<div className={mobileStyles.cards}>
+															<div
+																className={
+																	mobileStyles.portfolio
+																}
+															>
+																<b>{i.portfolio}</b>
+															</div>
+															<div className={mobileStyles.logo}>
+																{i.href !== "" && (
+																	<a
+																		href={i.href}
+																		target="_blank"
+																	>
+																		<img
+																			src={i.src}
+																			alt={data.name}
+																		/>
+																	</a>
+																)}
+																{i.href === "" && (
+																	<img
+																		src={i.src}
+																		alt={data.name}
+																	/>
+																)}
+															</div>
+														</div>
+													</>
+												);
+											})}
+										</>
+									);
+								})}
+							</>
+						);
+					})}
+				</div>
 			</MobileView>
 			<BrowserView>
+			<h2 className={styles.heading}>SPONSORS</h2>
 				{/* <Flex flexDirection="column" className={styles.container}>
 					<Center mb={5}>
 						<SponsorCarousel
@@ -35,10 +89,10 @@ const Sponsors = () => {
 					<Text className={styles.name}>SPONSORS</Text>
 				</Flex> */}
 				<div className={styles.container}>
-					
+					<div className={styles.sponsorList}>
 						{list.map((item: any) => {
 							return (
-								<div className={styles.sponsorList}>
+								<div className={styles.subcontainer}>
 									<div className={styles.title}>{item.title}</div>
 									<div className={styles.data}>
 										{item.data.map((data: any) => {
@@ -47,19 +101,36 @@ const Sponsors = () => {
 													{data.map((i: any) => {
 														return (
 															<div className={styles.cards}>
-																<div className={styles.portfolio}>{i.portfolio}</div>
-																<div className={styles.logo}><img src={i.src} alt={data.name} /></div>
+																<div
+																	className={styles.portfolio}
+																>
+																	{i.portfolio}
+																</div>
+																<div
+																	className={styles.logo}
+																>
+																	
+																	{i.href!=="" && <a href={i.href} target="_blank"><img
+																		src={i.src}
+																		alt={data.name}
+																	/></a>}
+																	{i.href===""&&
+																	<img
+																	src={i.src}
+																	alt={data.name}
+																/>}
+																</div>
 															</div>
-														)
+														);
 													})}
 												</div>
-											)
+											);
 										})}
 									</div>
 								</div>
-							)
+							);
 						})}
-
+					</div>
 				</div>
 			</BrowserView>
 		</>
--- src/pages/Sponsors/assets/content.ts
+++ src/pages/Sponsors/assets/content.ts
@@ -1,18 +1,20 @@
 import { config } from "../../../../config";
 
 const sponsors: any = [
-	// [
-	// 	{
-	// 		name: "RECAL",
-	// 		portfolio: "Nostalgia Partner",
-	// 		src: config.basePath + "/Assets/Images/SponsorImages",
-	// 	},
-	// ],
+	[
+		{
+			name: "RECAL",
+			portfolio: "Nostalgia Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/RECALLogo.png",
+			href: ""
+		},
+	],
 	[
 		{
 			name: "Ubon Headphones",
 			portfolio: "Title Sponsor",
 			src: config.basePath + "/Assets/Images/SponsorImages/UBON.png",
+			href: "https://ubon.in/"
 		},
 	],
 	[
@@ -20,38 +22,44 @@ const sponsors: any = [
 			name: "Royal Enfield",
 			portfolio: "Major Sponsor",
 			src: config.basePath + "/Assets/Images/SponsorImages/RoyalEnfield.png",
+			href: "https://www.royalenfield.com/"
 		},
 		{
 			name: "BharatX",
 			portfolio: "Co-sponsored by",
 			src: config.basePath + "/Assets/Images/SponsorImages/BharatX.png",
+			href: "https://bharatx.tech/"
 		},
 		{
 			name: "Hirect",
 			portfolio: "Powered by",
 			src: config.basePath + "/Assets/Images/SponsorImages/Hirect.png",
+			href: "https://www.hirect.in/"
 		},
 	],
 ];
 
 const proshowSponsors: any = [
 	[
-		// {
-		// 	name: "RealMe",
-		// 	portfolio: "Popular Nite Title Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
-		// {
-		// 	name: "KTM",
-		// 	portfolio: "Adventure Bikes Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
+		{
+			name: "RealMe",
+			portfolio: "Popular Nite Title Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/RealMeLogo.png",
+			href: ""
+		},
+		{
+			name: "KTM",
+			portfolio: "Adventure Bikes Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/KTMLogo.png",
+			href: ""
+		},
 		{
 			name: "Britannia Industries",
 			portfolio: "Comedy Nite Title Partner",
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/BritanniaIndustries.png",
+			href: "http://britannia.co.in/products/treat-croissant/cocoa-creme-roll"
 		},
 	],
 	[
@@ -59,17 +67,20 @@ const proshowSponsors: any = [
 			name: "Cothas Coffee",
 			portfolio: "Coffee Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/CothasCoffee.jpg",
+			href: "https://cothas.com/"
 		},
 		{
 			name: "City Union Bank",
 			portfolio: "Social Responsibility Partner",
 			src:
 				config.basePath + "/Assets/Images/SponsorImages/CityUnionBank.png",
+			href: "https://www.cityunionbank.com/"
 		},
 		{
 			name: "Coca Cola",
 			portfolio: "Beverage Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/CocaCola.png",
+			href: "https://www.coca-colacompany.com/"
 		},
 	],
 ];
@@ -80,18 +91,21 @@ const bankingSponsors: any = [
 			name: "Canara Bank",
 			portfolio: "Student Loans Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/CanaraBank.jpeg",
+			href: "https://www.twinbirds.co.in/"
 		},
-		// {
-		// 	name: "State Bank of India",
-		// 	portfolio: "YONO Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
+		{
+			name: "State Bank of India",
+			portfolio: "YONO Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/SBILogo.jpeg",
+			href: ""
+		},
 		{
 			name: "Union Bank of India",
 			portfolio: "Finance Partner",
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/UnionBankofIndia.jpeg",
+			href: "https://www.unionbankofindia.co.in/english/home.aspx",
 		},
 	],
 	[
@@ -99,12 +113,14 @@ const bankingSponsors: any = [
 			name: "Bank of Baroda",
 			portfolio: "Financial Services Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/BankOfBaroda.png",
+			href: ""
 		},
-		// {
-		// 	name: "ICICI Bank",
-		// 	portfolio: "Banking Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
+		{
+			name: "ICICI Bank",
+			portfolio: "Banking Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/ICICIBankLogo.jpeg",
+			href: ""
+		},
 	],
 ];
 
@@ -114,16 +130,19 @@ const fashionSponsors: any = [
 			name: "Emirates Modelling Company",
 			portfolio: "Modelling Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Emirates.jpg",
+			href: "https://www.instagram.com/emirates_fashion_company/?hl=en"
 		},
 		{
 			name: "Gobuyy",
 			portfolio: "Fashion Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Gobuyy.jpg",
+			href: ""
 		},
 		{
 			name: "ParX",
 			portfolio: "Casual Clothing Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/ParX.jpg",
+			href: "https://bit.ly/PARX_AW_21"
 		},
 	],
 	[
@@ -131,16 +150,19 @@ const fashionSponsors: any = [
 			name: "Beardo",
 			portfolio: "Mens Grooming Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Beardo.png",
+			href: "https://beardo.in/"
 		},
 		{
 			name: "Gatsby",
 			portfolio: "Styling Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Gatsby.jpg",
+			href: "https://www.gatsbyjs.com/"
 		},
 		{
 			name: "Derby",
 			portfolio: "Clothing partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Derby.jpg",
+			href: "https://www.derbymen.com/"
 		},
 	],
 	[
@@ -148,16 +170,19 @@ const fashionSponsors: any = [
 			name: "Van Heusen",
 			portfolio: "Apparels Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/VanHeusen.jpeg",
+			href: "https://www.vanheusenindia.com/"
 		},
 		{
 			name: "Twin Birds",
 			portfolio: "Apparel Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/TwinBirds.jpeg",
+			href: "https://www.twinbirds.co.in/"
 		},
 		{
 			name: "ayesha",
 			portfolio: "Accessories Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Ayesha.jpg",
+			href: "https://www.ayeshaaccessories.com/"
 		},
 	],
 	[
@@ -165,17 +190,20 @@ const fashionSponsors: any = [
 			name: "xctasy",
 			portfolio: "Jewellery Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/xctasy.jpeg",
+			href: "https://xctasy.co/"
 		},
 		{
 			name: "Reliance Trends",
 			portfolio: "Trends Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Trends.jpg",
+			href: "https://www.trends.ajio.com/"
 		},
 		{
 			name: "Wristbandwala",
 			portfolio: "Wearables Partner",
 			src:
 				config.basePath + "/Assets/Images/SponsorImages/Wristbandwala.png",
+			href: "https://wristbandwala.com/"
 		},
 	],
 ];
@@ -188,18 +216,21 @@ const educationSponsor: any = [
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/AceEngineeringAcademy.png",
+			href: "https://www.aceenggacademy.com/"
 		},
-		// {
-		// 	name: "TIME",
-		// 	portfolio: "Crack CAT Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
+		{
+			name: "TIME",
+			portfolio: "Crack CAT Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/TIMELogo.jpeg",
+			href: ""
+		},
 		{
 			name: "Byju's Careerlabs",
 			portfolio: "Higher Education Partner",
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/Byju'sCareerlabs.png",
+			href: "https://thecareerlabs.com/"
 		},
 	],
 	[
@@ -207,17 +238,20 @@ const educationSponsor: any = [
 			name: "SafeXpress",
 			portfolio: "Logistics Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/SafeXpress.png",
+			href: ""
 		},
 		{
 			name: "Scholastic Publications",
 			portfolio: "English Lits Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Scholastic.jpg",
+			href: "https://www.scholastic.co.in/"
 		},
-		// {
-		// 	name: "Indian Oil Corporation Limited",
-		// 	portfolio: "Industrial Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
+		{
+			name: "Indian Oil Corporation Limited",
+			portfolio: "Industrial Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/IndianOilCorporationLimitedLogo.jpeg",
+			href: ""
+		},
 	],
 	[
 		{
@@ -226,17 +260,20 @@ const educationSponsor: any = [
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/ShankarIASAcademy.jpg",
+			href: ""
 		},
 		{
 			name: "Qshala",
 			portfolio: "Curiosity Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Qshala.png",
+			href: ""
 		},
 		{
 			name: "Elearnmarkets",
 			portfolio: "Financial Education Partner",
 			src:
 				config.basePath + "/Assets/Images/SponsorImages/Elearnmarkets.jpg",
+			href: "https://www.elearnmarkets.com/"
 		},
 	],
 ];
@@ -247,16 +284,19 @@ const additonalSponsor: any = [
 			name: "YiPPee!",
 			portfolio: "Food Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Yipeee.png",
+			href: ""
 		},
 		{
 			name: "Nabati",
 			portfolio: "Wafer Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Nabati.png",
+			href: "https://www.nabatisnack.co.id/en/brand/product-wafer/detail/"
 		},
 		{
 			name: "StuCred",
 			portfolio: "Student Credit Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/StuCred.png",
+			href: "https://www.stucred.com/"
 		},
 	],
 	[
@@ -264,17 +304,20 @@ const additonalSponsor: any = [
 			name: "Gone Mad",
 			portfolio: "Choco Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/GoneMad.jpg",
+			href: "https://gpfoods.co.in/"
 		},
 		{
 			name: "Cavins",
 			portfolio: "Refreshments Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Cavins.jpeg",
+			href: "https://cavinkare.com/brand/cavinkare-dairy/cavins-milk/"
 		},
 		{
 			name: "Barbeque Nation",
 			portfolio: "Food Partner",
 			src:
 				config.basePath + "/Assets/Images/SponsorImages/BarbequeNation.png",
+			href: "https://www.barbequenation.com/"
 		},
 	],
 	[
@@ -282,17 +325,20 @@ const additonalSponsor: any = [
 			name: "Cornados",
 			portfolio: "Snacks Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Cornados.jpeg",
+			href: "https://snacatac.in/cornado/"
 		},
-		// {
-		// 	name: "Firstcry",
-		// 	portfolio: "Toddler Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
+		{
+			name: "Firstcry",
+			portfolio: "Toddler Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/FirstcryLogo.png",
+			href: ""
+		},
 		{
 			name: "Bharath Snacks",
 			portfolio: "Chikki Partner",
 			src:
 				config.basePath + "/Assets/Images/SponsorImages/BharathSnacks.jpg",
+			href: "https://bharathsnacks.in/"
 		},
 	],
 ];
@@ -303,33 +349,39 @@ const photographySponsors: any = [
 			name: "Sony",
 			portfolio: "Camera Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/SONY.png",
+			href: "https://www.sony.co.in/"
 		},
 		{
 			name: "KAPA",
 			portfolio: "Capture Partner ",
 			src: config.basePath + "/Assets/Images/SponsorImages/KAPA.png",
+			href: "https://kapa.co.in/"
 		},
 		{
 			name: "Fujifilm",
 			portfolio: "Photography Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/FujiFilm.png",
+			href: "https://global.fujifilm.com/en/"
 		},
-		// {
-		// 	name: "Indian Institute of Photography",
-		// 	portfolio: "Photography Partner",
-		// 	src: config.basePath + "/Assets/Images/SponsorImages",
-		// },
+		{
+			name: "Indian Institute of Photography",
+			portfolio: "Photography Partner",
+			src: config.basePath + "/Assets/Images/SponsorImages/IndianInstituteOfPhotographyLogo.jpeg",
+			href: ""
+		},
 	],
 	[
 		{
 			name: "Nestaway",
 			portfolio: "Stay Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Nestaway.png",
+			href: "https://www.nestaway.com/"
 		},
 		{
 			name: "Havells",
 			portfolio: "Appliance Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Havells.png",
+			href: "https://www.havells.com/"
 		},
 		{
 			name: "Terence Lewis Dance Academy",
@@ -337,11 +389,13 @@ const photographySponsors: any = [
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/TerenceLewisDanceAcademy.jpeg",
+			href: "https://www.terencelewis.com/"
 		},
 		{
 			name: "Hyperstack",
 			portfolio: "Certificate Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Hyperstack.png",
+			href: ""
 		},
 	],
 ];
@@ -352,11 +406,13 @@ const gamingPartners: any = [
 			name: "ASUS",
 			portfolio: "Gaming Cluster Title Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/ASUS.jpg",
+			href: "https://www.asus.com/in/"
 		},
 		{
 			name: "BalleBaazi",
 			portfolio: "Cricket Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/BalleBaazi.jpeg",
+			href: "https://www.ballebaazi.com/"
 		},
 	],
 ];
@@ -369,16 +425,19 @@ const outreachSponsor: any = [
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/LouisMusicPoint.png",
+			href: "https://maps.app.goo.gl/nj8sLRdTJyyGbEtt5"
 		},
 		{
 			name: "Unwind Centre",
 			portfolio: "Official GigaHertz Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Unwind.png",
+			href: "http://unwindcenter.org/"
 		},
 		{
 			name: "Diorama International Film Festival & market",
 			portfolio: "Film partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Diorama.jpg",
+			href: "https://www.diorama.in/"
 		},
 		{
 			name: "Swarnabhoomi Academy of Music",
@@ -386,6 +445,7 @@ const outreachSponsor: any = [
 			src:
 				config.basePath +
 				"/Assets/Images/SponsorImages/SwarnabhoomiAcademy.png",
+			href: "https://sam.org.in/"
 		},
 	],
 ];
@@ -396,21 +456,25 @@ const socialMediaSponsor: any = [
 			name: "Cherry store",
 			portfolio: "Unisex Jewellery partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/CherryStore.png",
+			href: "https://instagram.com/thecherrystore_india?igshid=YmMyMTA2M2Y="
 		},
 		{
 			name: "Paperish",
 			portfolio: "Gift Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Paperish.jpg",
+			href: "https://instagram.com/paperish__?igshid=YmMyMTA2M2Y="
 		},
 		{
 			name: "Craftscastle",
 			portfolio: "Crafts Castle",
 			src: config.basePath + "/Assets/Images/SponsorImages/CraftsCastle.jpg",
+			href: "https://instagram.com/_craft__castle?igshid=YmMyMTA2M2Y="
 		},
 		{
 			name: "Handlagetta",
 			portfolio: "Handmade Gifts Partner",
 			src: config.basePath + "/Assets/Images/SponsorImages/Handlagetta.png",
+			href: "https://instagram.com/handlagetta?igshid=YmMyMTA2M2Y="
 		},
 	],
 ];
